268ae7975dfc343d485992193521267a62a8563c
Marc <marcflament@free.fr>
2022-05-13 19:40:19 +0200

    Updated build

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
 
 export default [
   {
     input: ['./src/index.ts'],
-    external: ['react', 'react-dom'],
     output: {
       dir: 'dist',
       format: 'esm',
       sourcemap: false,
-      plugins: [terser()]
     },
     plugins: [
       typescript()
